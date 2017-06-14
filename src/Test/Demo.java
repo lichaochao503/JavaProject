9d796940d7ac2d5b813976a0817262a211c6420d
x6tim2plus <503990154@qq.com>
2017-06-15 00:14:23 +0800

    init

---
+++ src/Test/Demo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Test;
+
+/**
+ * @author LCC
+ *
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("我的第一个java");
+	}
+
+}
